a7fdabd8195b0c4122449ce6500e64bc4a63c83d
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-10 12:20:04 +0100

    oef11
    o

---
+++ oef/oef11.mjs
@@ -0,0 +1,19 @@
+
+/*let som = 0;
+let getal = 3;
+
+
+while(som <= 12){
+    console.log(som);
+    som += getal;
+}
+*/
+
+let getal = 5;
+let som = 0;
+
+while(som != 100){
+   console.log(som);
+   som += getal;
+}
+process.exit();
