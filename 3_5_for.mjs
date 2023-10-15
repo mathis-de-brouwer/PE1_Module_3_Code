3134190d843a4c516f1573e93ee1d42f1913de62
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-15 12:43:16 +0100

    example

--- 3_5_for.mjs
+++ 3_5_for.mjs
@@ -5,3 +5,16 @@ for(let i = 1; i <= 50; i++){
 }
 
 console.log(som);
+
+/*for(initialization; condition; iteration){
+    codeblok;
+}
+
+let variabele = 0; // Beginwaarde meegeven, initialization
+while(condition){
+    codeblok;
+    variabele++; // Iteration
+}
+
+
+*/
